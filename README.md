03aada07e9685781ac97519633c4040a764f7e3d
Vinayak Sharma <oz@Vinayaks-MacBook-Pro.local>
2025-07-24 12:32:59 +0530

    Refactoring, Added Usage Info, Added comments

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-A sample command-line application providing basic argument parsing with an entrypoint in `bin/`.
+## kata1
+
+Implementation of `kata1` as referenced from [https://osherove.com/tdd-kata-1/].
+
+### Entrypoint
+./bin/kata1.dart
+
+### Execution
+dart run ./bin/data1.dart
+
+### Usage Help
+dart run ./bin/data1.dart --help
--- bin/kata1.dart
+++ bin/kata1.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:args/args.dart';
 
-const String version = '0.0.1';
+const String version = '1.0.1';
 
 ArgParser buildParser() {
   return ArgParser()
@@ -21,8 +21,30 @@ ArgParser buildParser() {
     ..addFlag('version', negatable: false, help: 'Print the tool version.');
 }
 
+// Print usage information for the command-line tool.
 void printUsage(ArgParser argParser) {
   print('Usage: dart kata1.dart <flags> [arguments]');
+  print('<-------      ------->');
+  print('Function: add numbers from a string input');
+  print('Function: new lines work as delimiters');
+  print("Function: multiline arguments can be specified as\n'''\n<input>\n'''");
+  print(
+    'Function: specify custom delimiters using "//<delimiter>" in the first line',
+  );
+  print(
+    'Function: multicharacter delimiters are supported with "//[<delimiter>]"',
+  );
+  print(
+    'Function: multiple custom delimiters can be specified with "//[<delimiter1>][<delimiter2>]"',
+  );
+  print('<-------      ------->');
+  print('Constraint: negative numbers will throw an exception');
+  print('Constraint: numbers greater than 1000 will be ignored');
+  print('Constraint: empty strings will be ignored');
+  print('Constraint: numbers must be integers');
+  print(r'Constraint: "[" and "]" are reserved for custom delimiters');
+  print('<-------      ------->');
+  print('Flags:');
   print(argParser.usage);
 }
 
@@ -33,7 +55,6 @@ void main(List<String> arguments) {
     bool verbose = false;
     late String input;
 
-    // Process the parsed arguments.
     if (results.flag('help')) {
       printUsage(argParser);
       return;
@@ -46,17 +67,15 @@ void main(List<String> arguments) {
       verbose = true;
     }
 
-    // Act on the arguments provided.
     if (verbose) {
       print('[VERBOSE] All arguments: ${results.arguments}');
     }
-    // Check if the input string is provided.
+
     if (results.rest.isEmpty) {
       print('No arguments provided. Please provide a string to add.');
       // Read input from stdin if no arguments are provided.
       List<String> lines = [];
-      String?
-      line; // Use nullable String to handle potential null from readLineSync()
+      String? line;
 
       do {
         line = stdin.readLineSync();
@@ -66,26 +85,37 @@ void main(List<String> arguments) {
       } while (line != null && line.isNotEmpty);
       input = lines.join('\n');
     } else {
-      // Use the first argument as input.
-      input = results.rest.first;
+      input = results.rest.join('\n');
     }
     // Call `add` function with the input string.
-    print(add(input));
+    print(add(input, verbose: verbose));
   } on FormatException catch (e) {
     // Print usage information if an invalid argument was provided.
     print(e.message);
     print('');
-    printUsage(argParser);
+    print('Use --help for usage information.');
   }
 }
 
-int add(String numbers) {
+// add numbers from a list of integers
+// returns the sum of the numbers
+// throws FormatException if negative numbers are found
+// ignores numbers greater than 1000
+int add(String numbers, {bool? verbose}) {
   if (numbers.isEmpty) {
     return 0;
   }
-  return addNumbers(extractNumbers(numbers));
+  final extractedNumbers = extractNumbers(numbers.trim(), verbose: verbose);
+  final negatives = extractedNumbers.where((n) => n < 0).toList();
+  if (extractedNumbers.isNotEmpty) {
+    throw FormatException('negatives not allowed: ${negatives.join(', ')}');
+  }
+  return extractedNumbers.fold(0, (a, b) => b < 1000 ? a + b : a);
 }
 
+// extract the delimiter from the input string
+// returns a list of delimiters
+// if no custom delimiter is found, returns a list with the default delimiter ','
 List<String> extractDelimiter(String input) {
   final RegExp delimiterPattern = RegExp(r'^\/\/(.)\n(.|\n)*');
   final RegExp customDelimiterPattern = RegExp(r'^\/\/\[(.*)\]\n(.|\n)*');
@@ -99,21 +129,22 @@ List<String> extractDelimiter(String input) {
   return [','];
 }
 
-List<int> extractNumbers(String input) {
+// extract numbers from the input string
+// returns a list of integers
+// if verbose is true, prints the detected delimiters and extracted numbers
+List<int> extractNumbers(String input, {bool? verbose}) {
   if (input.startsWith('//')) {
-    final delimiter = extractDelimiter(input);
+    final delimiters = extractDelimiter(input);
+    if (verbose == true) {
+      print('[VERBOSE] Detected delimiters: $delimiters');
+    }
     final buffer = input.split('\n').sublist(1).join(',');
-    final bufferList = buffer.split(RegExp('[${delimiter.join('')},]'));
+    final bufferList = buffer.split(RegExp('[${delimiters.join('')},]'));
+    if (verbose == true) {
+      print('[VERBOSE] Extracted numbers: $bufferList');
+    }
     return bufferList.map(int.parse).toList();
   } else {
     return input.split('\n').join(',').split(',').map(int.parse).toList();
   }
-}
-
-int addNumbers(List<int> numbers) {
-  final negatives = numbers.where((n) => n < 0).toList();
-  if (negatives.isNotEmpty) {
-    throw FormatException('negatives not allowed: ${negatives.join(', ')}');
-  }
-  return numbers.fold(0, (a, b) => b < 1000 ? a + b : a);
 }
